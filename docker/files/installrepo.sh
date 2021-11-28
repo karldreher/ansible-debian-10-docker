a9b92f55fba323208f0e06398cad6e8369600fea
Karl Dreher <karldreher@outlook.com>
2021-11-28 10:35:20 -0700

    better, no shell script

--- docker/files/installrepo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo \
-    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
